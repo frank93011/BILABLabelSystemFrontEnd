05b03fbb18663c417a56096bc3739c3dcbf7f76e
Frank Chen <biohazard93011@ntu.im>
2021-02-26 23:41:44 +0800

    add login verifycation

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,21 @@ import {
   Link,
   useRouteMatch
 } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function Header() {
   let { url } = useRouteMatch();
+  // dispatch redux logout action
+  const dispatch = useDispatch();
+  const logout = () => {
+        console.log("clearLoginInfo")
+        dispatch({
+            type: 'LOGOUT'
+        });
+    };
+
+  const profileObj = useSelector(state => state.profileObj);
   return (
     <div id="Header" className="justify-around">
       <div className="justify-start header-logo-container">
@@ -18,7 +30,8 @@ function Header() {
       <div className="justify-end header-router-container">
         <Link to={`${url}/Label`}><div className="header-router-button">標記</div></Link>
         <Link to={`${url}/Validation`}><div className="header-router-button">驗證</div></Link>
-        <div className="header-router-button w-120"><span className="f-25 mb-5">Hi,</span> 詩筠</div>
+        <div className="header-router-button w-120"><span className="f-25 mb-5">Hi,</span> {profileObj.givenName}</div>
+        <img className="user-image" src={profileObj.imageUrl} alt="" onClick={logout}/>
       </div>
     </div>
   )
